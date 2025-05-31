f628c25796fd4834ba79c17179b0bdfe7d7ab53a
rjnetwork212 <jelitanadea77@gmail.com>
2025-06-01 03:05:56 +0700

    Add files via upload

---
+++ kuntul.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./rjmin --algo progpowz --url stratum+tcp://pool.woolypooly.com:3146 --worker RJ --user iZ2q2xfw9AdX8YpGrcrjEPTG2ie8FMuXMFdDNKqRRbGo15zbuUfMAzDbtEDxcDpJcXGijaADG2WVs41p8PMiBnzrV95mE2xHNXM39WpQjPeD --pass x
+sleep 5
+done
